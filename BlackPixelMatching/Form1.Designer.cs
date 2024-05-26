5cd0651bbd649139e6fe2c24c9d1cd0056ab9b65
Marlore <68165023+Marlore@users.noreply.github.com>
2024-05-26 12:55:14 +0300

    Initial commit

---
+++ BlackPixelMatching/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace BlackPixelMatching
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            listBox1 = new ListBox();
+            openFileDialog1 = new OpenFileDialog();
+            label1 = new Label();
+            PathExplorer = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(273, 314);
+            button1.Name = "button1";
+            button1.Size = new Size(158, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Посчитать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(2, 94);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(795, 214);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(187, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Укажите путь:";
+            // 
+            // PathExplorer
+            // 
+            PathExplorer.BackColor = SystemColors.ControlLight;
+            PathExplorer.Image = (Image)resources.GetObject("PathExplorer.Image");
+            PathExplorer.Location = new Point(470, 51);
+            PathExplorer.Name = "PathExplorer";
+            PathExplorer.Size = new Size(39, 34);
+            PathExplorer.TabIndex = 3;
+            PathExplorer.UseVisualStyleBackColor = false;
+            PathExplorer.Click += PathExplorer_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(282, 58);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(182, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(PathExplorer);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private OpenFileDialog openFileDialog1;
+        private Label label1;
+        private Button PathExplorer;
+        private TextBox textBox1;
+    }
+}
---
+++ BlackPixelMatching/Form1.cs
@@ -0,0 +1,129 @@
+using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+
+namespace BlackPixelMatching
+{
+    public partial class Form1 : Form
+    {
+        string FilePath;
+        Dictionary<int,string> ConvertList;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            
+            Convert();
+            var dic =ConvertList.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var item in dic)
+                listBox1.Items.Add(item.Value);
+
+        }
+        
+        private async void Convert()
+        {
+            List<Thread> tasks = new List<Thread>();
+            ConvertList = new Dictionary<int, string>();
+            using (var rasterizer = new GhostscriptRasterizer())
+            {
+                rasterizer.GraphicsAlphaBits = (int)GhostscriptImageDeviceAlphaBits.V_4;
+                rasterizer.TextAlphaBits = (int)GhostscriptImageDeviceAlphaBits.V_4;
+                rasterizer.Open(FilePath);
+                int pageCount = rasterizer.PageCount;
+                int PageToThread = 20;
+                int taskCount = (int)Math.Ceiling((float)pageCount / PageToThread);
+                
+                for (int i = 0; i < taskCount; i++)
+                {
+
+                    if ((i + 1) * PageToThread > pageCount && i * PageToThread < pageCount)
+                    {
+                        int startPage = (i * PageToThread) + 1;
+                        var thread = new Thread(()=> RenderCounter(startPage, pageCount, rasterizer));
+                        thread.Start();
+                        tasks.Add(thread);
+                    }
+
+                    else if((i + 1) * PageToThread <= pageCount)
+                    {
+                        int startPage = (i * PageToThread) + 1;
+                        int endPage = (i + 1) * PageToThread;
+                        var thread = new Thread(() => RenderCounter(startPage, endPage, rasterizer));
+                        thread.Start();
+                        tasks.Add(thread);
+                    }
+                }
+                foreach (var task in tasks)
+                {
+                    task.Join();
+                }
+
+                rasterizer.Close();
+                rasterizer.Dispose();
+            }
+           
+        }
+        private void RenderCounter(int from,int to, GhostscriptRasterizer rasterizer)
+        {
+            for (int pageNumber = from; pageNumber <= to; pageNumber++)
+            {
+                var outputFormat = ImageFormat.Png;
+                var image = rasterizer.GetPage(300, pageNumber);
+                var bitmap = new Bitmap(image);
+                var count = BlackPixelCount(bitmap);
+                ConvertList.Add(pageNumber, Path.GetFileName(FilePath) + " Page:" + pageNumber + " Black pixel percent: " + (count * 100) + "%");
+                
+
+            }
+        }
+
+        private float BlackPixelCount(Bitmap bmp)
+        {
+            int blackColor = 0;
+            int count = 0;
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color color = bmp.GetPixel(x, y);
+                    count++;
+                    if (color.ToArgb() != Color.White.ToArgb())
+                        blackColor++;
+                }
+
+            }
+            return (float)blackColor/count;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void PathExplorer_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+
+            // Set the file dialog to filter for graphics files.
+            this.openFileDialog1.Filter = "Pdf Files|*.pdf";
+
+            // Allow the user to select multiple images.
+            this.openFileDialog1.Multiselect = false;
+            this.openFileDialog1.ShowDialog();
+            var fileName = this.openFileDialog1.FileName;
+            textBox1.Text = fileName;
+            FilePath =fileName;
+
+        }
+    }
+}
